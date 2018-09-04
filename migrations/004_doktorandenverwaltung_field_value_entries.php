591ae39f0e7dfe913b17d5555119bcbcbc055fe6
asudau <sudau@elan-ev.de>
2018-09-04 15:37:44 +0200

    fix #26

--- migrations/004_doktorandenverwaltung_field_value_entries.php
+++ migrations/004_doktorandenverwaltung_field_value_entries.php
@@ -11,31 +11,26 @@ class DoktorandenverwaltungFieldValueEntries extends Migration
     {
         $db = DBManager::get();
 
-        $query = "INSERT INTO `doktorandenverwaltung_field_values` (`id`, `field_id`, `his_id`, `lid`, `uniquename`, `defaulttext`, `astat_bund`) 
+        $query = "INSERT INTO `doktorandenverwaltung_field_values` (`field_id`, `his_id`, `lid`, `uniquename`, `defaulttext`, `astat_bund`) 
             VALUES 
-            (NULL, 'ef005', '1', NULL, '1', 'männlich', NULL), 
-            (NULL, 'ef005', '2', NULL, '2', 'weiblich', NULL),
-            (NULL, 'ef008', '000', NULL, 000, 'Deutschland', NULL),
-            (NULL, 'ef012', '1', NULL, '1', 'Erstregistrierung', NULL),
-            (NULL, 'ef012', '2', NULL, '2', 'Neuregistrierung', NULL),
-            (NULL, 'ef015', '1', NULL, '1', 'Ja', NULL),
-            (NULL, 'ef015', '0', NULL, '0', 'Nein', NULL),
-            (NULL, 'ef016', '1', NULL, '1', 'Ja', NULL),
-            (NULL, 'ef016', '0', NULL, '0', 'Nein', NULL),
-            (NULL, 'ef017', '1', NULL, '1', 'Ja', NULL),
-            (NULL, 'ef017', '0', NULL, '0', 'Nein', NULL),
-            (NULL, 'ef018', '1', NULL, '1', 'Monografie', NULL),
-            (NULL, 'ef018', '2', NULL, '2', 'publikationsbasierte/kumulative Dissertation', NULL),
-            (NULL, 'ef021', '1', NULL, 'SoSe', 'Sommersemester', NULL),
-            (NULL, 'ef021', '2', NULL, 'WiSe', 'Wintersemester', NULL),
-            (NULL, 'ef023', '0', NULL, '0', 'wurde noch nicht abgelegt', NULL),
-            (NULL, 'ef023', '1', NULL, '1', 'wurde abgelegt und bestanden', NULL)
+            ('ef005', '1', NULL, '1', 'männlich', NULL), 
+            ('ef005', '2', NULL, '2', 'weiblich', NULL),
+            ('ef012', '1', NULL, '1', 'Erstregistrierung', NULL),
+            ('ef012', '2', NULL, '2', 'Neuregistrierung', NULL),
+            ('ef015', '1', NULL, '1', 'Ja', NULL),
+            ('ef015', '0', NULL, '0', 'Nein', NULL),
+            ('ef018', '128', NULL, '1', 'Monografie', NULL),
+            ('ef018', '129', NULL, '2', 'publikationsbasierte/kumulative Dissertation', NULL),
+            ('ef018', '99', NULL, '0', 'undefiniert (von Migration)', NULL),
+            ('ef021', '1', NULL, 'SoSe', 'Sommersemester', NULL),
+            ('ef021', '2', NULL, 'WiSe', 'Wintersemester', NULL),
+            ('ef023', '2', NULL, '2', 'endgültig nicht bestanden', NULL),
+            ('ef023', '1', NULL, '1', 'bestanden', NULL),
+            ('ef023', '18', NULL, '18', 'Nicht angeboten / Keine Angabe', NULL)
             ";
         
         $db->exec($query);
         
-        
-        SimpleORMap::expireTableScheme();
     }
 
     public function down()

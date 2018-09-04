04b3d6fa6dabd2f3f5a8a72d91c163614a95f1fa
asudau <sudau@elan-ev.de>
2018-09-04 17:17:00 +0200

    fix #55

--- migrations/004_doktorandenverwaltung_field_value_entries.php
+++ migrations/004_doktorandenverwaltung_field_value_entries.php
@@ -24,8 +24,8 @@ class DoktorandenverwaltungFieldValueEntries extends Migration
             ('ef018', '99', NULL, '0', 'undefiniert (von Migration)', NULL),
             ('ef021', '1', NULL, 'SoSe', 'Sommersemester', NULL),
             ('ef021', '2', NULL, 'WiSe', 'Wintersemester', NULL),
-            ('ef023', '2', NULL, '2', 'endgültig nicht bestanden', NULL),
-            ('ef023', '1', NULL, '1', 'bestanden', NULL),
+            ('ef023', '0', NULL, NULL, 'wurde noch nicht abgelegt', '0'),
+            ('ef023', '1', NULL, NULL, 'wurde abgelegt und bestanden', '1'),
             ('ef023', '18', NULL, '18', 'Nicht angeboten / Keine Angabe', NULL)
             ";
         

aeaa4c0606d2bcb4891f51d9d26b76a357128996
asudau <sudau@elan-ev.de>
2018-09-05 15:11:54 +0200

    rel #61

--- migrations/004_doktorandenverwaltung_field_value_entries.php
+++ migrations/004_doktorandenverwaltung_field_value_entries.php
@@ -13,20 +13,20 @@ class DoktorandenverwaltungFieldValueEntries extends Migration
 
         $query = "INSERT INTO `doktorandenverwaltung_field_values` (`field_id`, `his_id`, `lid`, `uniquename`, `defaulttext`, `astat_bund`) 
             VALUES 
-            ('ef005', '1', NULL, '1', 'männlich', NULL), 
-            ('ef005', '2', NULL, '2', 'weiblich', NULL),
-            ('ef012', '1', NULL, '1', 'Erstregistrierung', NULL),
-            ('ef012', '2', NULL, '2', 'Neuregistrierung', NULL),
-            ('ef015', '1', NULL, '1', 'Ja', NULL),
-            ('ef015', '0', NULL, '0', 'Nein', NULL),
-            ('ef018', '128', NULL, '1', 'Monografie', NULL),
-            ('ef018', '129', NULL, '2', 'publikationsbasierte/kumulative Dissertation', NULL),
-            ('ef018', '99', NULL, '0', 'undefiniert (von Migration)', NULL),
-            ('ef021', '1', NULL, 'SoSe', 'Sommersemester', NULL),
-            ('ef021', '2', NULL, 'WiSe', 'Wintersemester', NULL),
-            ('ef023', '0', NULL, NULL, 'wurde noch nicht abgelegt', '0'),
-            ('ef023', '1', NULL, NULL, 'wurde abgelegt und bestanden', '1'),
-            ('ef023', '18', NULL, '18', 'Nicht angeboten / Keine Angabe', NULL)
+            ('geschlecht', '1', NULL, '1', 'mÃ¤nnlich', NULL), 
+            ('geschlecht', '2', NULL, '2', 'weiblich', NULL),
+            ('art_reg_prom', '1', NULL, '1', 'Erstregistrierung', NULL),
+            ('art_reg_prom', '2', NULL, '2', 'Neuregistrierung', NULL),
+            ('immatrikulation', '1', NULL, '1', 'Ja', NULL),
+            ('immatrikulation', '0', NULL, '0', 'Nein', NULL),
+            ('art_dissertation', '128', NULL, '1', 'Monografie', NULL),
+            ('art_dissertation', '129', NULL, '2', 'publikationsbasierte/kumulative Dissertation', NULL),
+            ('art_dissertation', '99', NULL, '0', 'undefiniert (von Migration)', NULL),
+            ('semester', '1', NULL, 'SoSe', 'Sommersemester', NULL),
+            ('semester', '2', NULL, 'WiSe', 'Wintersemester', NULL),
+            ('status_abschlusspruefung', '0', NULL, NULL, 'wurde noch nicht abgelegt', '0'),
+            ('status_abschlusspruefung', '1', NULL, NULL, 'wurde abgelegt und bestanden', '1'),
+            ('status_abschlusspruefung', '18', NULL, '18', 'Nicht angeboten / Keine Angabe', NULL)
             ";
         
         $db->exec($query);

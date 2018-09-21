0d5a3f9db45b9189f739904c2d8c2fe2af707780
asudau <sudau@elan-ev.de>
2018-09-21 13:38:44 +0200

    fix #63

--- migrations/002_doktorandenverwaltung_add_fields_table.php
+++ migrations/002_doktorandenverwaltung_add_fields_table.php
@@ -35,7 +35,7 @@ class DoktorandenverwaltungAddFieldsTable extends Migration
            ('hochschule_prom', 'Hochschule der Promotion', '0530', NULL, NULL, NULL, 'auto', 'university', 'lid', 'ef003'),
            ('paginiernummer', 'Paginiernummer', NULL, NULL, NULL, NULL, 'auto', NULL, NULL, 'ef004'),
            ('id', 'Paginiernummer_int', NULL, NULL, NULL, NULL, 'auto', NULL, NULL, 'Paginiernummer_int'),
-           ('geschlecht', 'Geschlecht', NULL, '3', 'doktorandendaten', '1', 'manual_req', NULL, 'his_id', 'ef005'),
+           ('geschlecht', 'Geschlecht', NULL, NULL, 'doktorandendaten', '1', 'manual_req', NULL, 'his_id', 'ef005'),
            ('geburtsdatum_tag', 'Geburtsdatum - Tag', NULL, NULL, NULL, NULL, 'manual_req', NULL, NULL, 'ef006u1'),
            ('geburtsdatum_monat', 'Geburtsdatum - Monat', NULL, NULL, NULL, NULL, 'manual_req', NULL, NULL, 'ef006u2'),
            ('geburtsdatum_jahr', 'Geburtsdatum - Jahr', NULL, NULL, NULL, NULL, 'manual_req', NULL, NULL, 'ef006u3'),
@@ -45,13 +45,13 @@ class DoktorandenverwaltungAddFieldsTable extends Migration
            ('staatsangehoerigkeit', 'Staatsangehörigkeit', NULL, NULL, 'doktorandendaten', '8', 'manual_req', 'country', 'his_id', 'ef008'),
            ('weitere_staatsangehoerigkeit', 'Weitere Staatsangehörigkeit', NULL, NULL, 'doktorandendaten', '9', 'manual_opt', 'country', 'his_id', 'ef009'),
            ('art_promotion', 'Art der Promotion', NULL, NULL, 'promotionsdaten', '2', 'manual_req', NULL, 'his_id', 'ef010'),
-           ('promotionsfach', 'Promotionsfach', NULL, '5', 'promotionsdaten', '3', 'manual_req', NULL, 'lid', 'ef011'),
+           ('promotionsfach', 'Promotionsfach', NULL, '4', 'promotionsdaten', '3', 'manual_req', NULL, 'lid', 'ef011'),
            ('art_reg_prom', 'Art der Registrierung', NULL, NULL, 'promotionsdaten', '4', 'manual_req', 'candidate_status', 'his_id', 'ef012'),
            ('promotionsbeginn_monat', 'Promotionsbeginn - Monat', NULL, NULL, 'promotionsdaten', '5', 'manual_req', 'monat', 'id', 'ef013u1'),
            ('promotionsbeginn_jahr', 'Promotionsbeginn - Jahr', NULL, NULL, 'promotionsdaten', '6', 'manual_req', NULL, NULL, 'ef013u2'),
            ('promotionsende_monat', 'Ende der Promotion - Monat', NULL, NULL, 'promotionsdaten', '7', 'manual_opt', 'monat', 'id', 'ef014u1'),
            ('promotionsende_jahr', 'Ende der Promotion - Jahr', NULL, NULL, 'promotionsdaten', '8', 'manual_opt', NULL, NULL, 'ef014u2'),
-           ('matrikelnummer', 'Matrikelnummer', NULL, NULL, 'promotionsdaten', '10', 'manual_opt', NULL, NULL, '0'),
+           ('matrikelnummer', 'Matrikelnummer', NULL, '5', 'promotionsdaten', '10', 'manual_opt', NULL, NULL, '0'),
            ('immatrikulation', 'Immatrikulation', NULL, NULL, 'promotionsdaten', '9', 'manual_req', 'ja_nein', 'his_id', 'ef015'),
            ('struk_promotionsprogramm', 'Teilnahme an einem strukturierenden Promotionsprogramm', NULL, NULL, 'promotionsdaten', '11', 'manual_req', 'structured_program', 'lid', 'ef016'),
            ('employment', 'Beschäftigungsverhältnis an der Hochschule der Promotion', NULL, NULL, 'promotionsdaten', '12', 'manual_req', 'ja_nein', 'his_id', 'ef017'),
@@ -82,7 +82,7 @@ class DoktorandenverwaltungAddFieldsTable extends Migration
            ('ef036', 'frei für landesinterne Angaben', NULL, NULL, NULL, NULL, 'manual_opt', NULL, NULL, '1'),
            ('ef037', 'frei für landesinterne Angaben', NULL, NULL, NULL, NULL, 'manual_opt', NULL, NULL, '1'),
            ('ef038', 'frei für landesinterne Angaben', NULL, NULL, NULL, NULL, 'manual_opt', NULL, NULL, '1'),
-           ('geburtstag', 'Geburtsdatum', NULL, '4', 'doktorandendaten', '2', 'manual_req', NULL, NULL, '0')
+           ('geburtstag', 'Geburtsdatum', NULL, '3', 'doktorandendaten', '2', 'manual_req', NULL, NULL, '0')
            ";
         
         $db->exec($query);

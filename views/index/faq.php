812c0ce6dfb961946e7676ad68f628117bddc05b
asudau <sudau@elan-ev.de>
2018-10-01 17:18:09 +0200

    fix #67

--- views/index/faq.php
+++ views/index/faq.php
@@ -1,1 +1,24 @@
 <h1> FAQ - Fragen und Antworten </h1>
+
+<h2>Allgemeines</h2>
+
+<h3>Gibt es eine Rechtsgrundlage für die Erhebung der Promovierendendaten? </h3>
+    <p>Mit der Novellierung des Hochschulstatistikgesetzes (2017) wurde neben der Studierenden- und Prüfungsstatistik, die Promovierendenstatistik gesetzlich verankert. Diese Erhebung wurde erstmals für das Berichtsjahr 2017 durchgeführt. </p>
+    <p>Rechtsgrundlage: <a href='https://www.statistik.niedersachsen.de/download/116768'>Hochschulstatistikgesetz - HStatG </a></p>
+<h3>Zu welchem Stichtag muss gemeldet werden? </h3>
+    <p>Grundsätzlich ist jährlich zum 1. Dezember der aktuelle Bestand an Promovierenden zu melden, sowie alle Promovierenden, die seit der letzten Meldung ihre Promotion abgebrochen, unterbrochen oder beendet haben. Die Daten können ganzjährig im Modul Doktorandenverwaltung gepflegt werden. </p>
+<h3>Gibt es eine Import-/Exportfunktion für die Promovierendendaten? </h3>
+    <p>Der Import der Promovierendendaten aus fachbereichspezifischen Excel-Tabellen ist leider nicht möglich. Neue Promovierende müssen über die Eingabemaske erfasst werden, Änderungen an bestehenden Daten über den Bearbeitungsmodus eingepflegt werden. Für den Zweck des Weiterverarbeitens kann jedoch eine Liste aus Stud.IP mit den eingegeben Werten exportiert werden. </p>
+<h3>Was ist der Unterschied zwischen einer Registrierung und der Immatrikulation? </h3>
+    <p>Die Registrierung aller Promovierenden ist gesetzlich vorgeschrieben. In der Registrierung werden lediglich die für die Statistik erforderlichen Daten abgefragt. Zudem können Promovierende die Immatrikulation (Dezernat 5) beantragen, wenn Sie den Studierendenstatus (inkl. Semesterticket) erhalten möchten.</p>
+<h3>Müssen auch Daten für Promovierende eingetragen werden, die noch immatrikulieren oder sich bereits immatrikuliert haben? </h3>
+    <p>Ja, sowohl für die immatrikulierten als auch nicht immatrikulierten Promovierenden müssen die Daten vollständig eingetragen werden, da es noch keinen automatisierten Abgleich der Daten aus dem Campus Management System gibt. </p>
+<h3>Wo finde ich weitere Hinweise zur Promovierendenstatistik? </h3>
+<p>Das Landesamt für Statistik Niedersachsen stellt aktuelle Schlüsselverzeichnisse, Datensatzbeschreibungen und Zuordnungstabellen unter folgendem <a href="https://www.statistik.niedersachsen.de/themenbereiche/bildung/hochschulen/themenbereich-bildung---hochschulstatistik-hochschulfinanzstatistik-132143.html">Link</a> bereit. </p>
+
+<h2>Ansprechpartner </h2>
+Bei Fragen … <br>
+… zur Registrierung in Stud.IP: <br>
+<a href="mailto:doktorandenverwaltung@uni-osnabrueck.de">doktorandenverwaltung@uni-osnabrueck.de</a><br>
+… zur statistischen Meldung: <br>
+<a href="mailto:zbw@uni-osnabrueck.de">zbw@uni-osnabrueck.de</a> <br>

25694fdd8777440d43e3be2b06ebf73de2c02896
asudau <sudau@elan-ev.de>
2018-10-24 13:33:46 +0200

    cleanup

--- models/DoktorandenEntry.class.php
+++ models/DoktorandenEntry.class.php
@@ -106,13 +106,6 @@ class DoktorandenEntry extends \SimpleORMap
         parent::configure($config);
     }
     
-    //für neu erzeugte Einträge müssen einige Werte initialisiert werden
-    public function setup(){
-        //Paginierung ist ein String-Wert der aus der id erzeugt wird weil fortlaufend
-//        $this->ef004 = str_pad($this->id, 6 ,'0', STR_PAD_LEFT);
-        $this->store();
-    }
-    
     public function getFields() {
         return $this->db_fields;
     }

29d89390568c39fa072fbe15c831e256091c5219
asudau <sudau@elan-ev.de>
2019-01-07 17:25:56 +0100

    fix #197

--- views/index/index.php
+++ views/index/index.php
@@ -7,7 +7,7 @@
 <table id='doktoranden-entries' class="sortable-table default">
     <thead>
 		<tr>
-            <th data-sort="false" style='width:10%'>Aktionen</th>
+            <th data-sort="false" style='width:5%'></th>
             <?php foreach($fields as $field): ?>
                 <?php if ($field->id == 'geburtstag' || $field->id == 'chdate') : ?>
                     <th data-sort="htmldata"><span><?= $field['title'] ?></span></th>   
@@ -15,7 +15,8 @@
                     <th data-sort="text"><span><?= $field['title'] ?></span></th>   
                 <?php endif ?>
             <?php endforeach ?>
-            <th data-sort="text" style='width:10%'>Status</th>    
+            <th data-sort="text" style='width:10%'>Status</th>  
+            <th data-sort="false" style='width:5%'></th>
         </tr>
     </thead>
     <tbody>
@@ -23,10 +24,7 @@
     <tr>
 
         <td><a href='<?=$this->controller->url_for('index/edit/' . $entry['id']) ?>' title='Eintrag editieren' data-dialog="size=big"><?=Icon::create('edit')?></a>
-        <?php if(RolePersistence::isAssignedRole($GLOBALS['user']->user_id,
-                                                            Doktorandenverwaltung::DOKTORANDENVERWALTUNG_ADMIN_ROLE)): ?>
-            <a onclick="return confirm('Eintrag löschen?')" href='<?=$this->controller->url_for('index/delete/' . $entry['id']) ?>' title='Eintrag löschen' ><?=Icon::create('trash')?></a>
-        <?php endif ?>    
+        
         <br/></td>
         <?php foreach($fields as $field): ?>
             <?php if ($field->value_key) : ?>
@@ -43,7 +41,12 @@
 
         <?php endforeach ?>
         <td title='Noch <?= $entry->numberRequiredFields()-$entry->completeProgress() ?> fehlende Einträge'><?= round($entry->completeProgress()/$entry->numberRequiredFields(), 2)*100 ?>%</td>
-        
+         <?php if(RolePersistence::isAssignedRole($GLOBALS['user']->user_id,
+                                                            Doktorandenverwaltung::DOKTORANDENVERWALTUNG_ADMIN_ROLE)): ?>
+             <td>
+                 <a onclick="return confirm('Eintrag löschen?')" href='<?=$this->controller->url_for('index/delete/' . $entry['id']) ?>' title='Eintrag löschen' ><?=Icon::create('trash')?></a>
+             </td>
+         <?php endif ?>   
     </tr>
     <?php endforeach ?>
     </tbody>
